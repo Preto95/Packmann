187138d1eb9cde0a98c4d0cad4a62947a155d7a9
PretoX <sebastian-giese@posteo.de>
2018-02-16 18:35:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<p><h1>Packmann.sh</h1></p>
+<p><h3>The package manager suite especially for newbies</h3></p>
+<p>It is not a mistake, if you are new to Linux! We all began as newbies. This little bash script may helps you to learn how to use the package manager on command line or what you can do with that.</p>
+
+<p>All what you have to do is to download this thing.</p>
+<p>At 1st install git on your system with root permissions:</p>
+
+<p>Fedora</p>
+<p><code># dnf install git</code></p>
+<p>Debian-based systems</p>
+<p><code># apt install git</code></p>
+<p>Arch-based systems</p>
+<p><code># pacman -S git</code></p>
+<p>CentOS / RHEL systems</p>
+<p><code># yum install git</code></p>
+
+
+<p>At 2nd download this repo:</p>
+<p><code>$ git clone </code></p>
+
+<p>At 3rd run the script with root permissions:</p>
+<p><code># ./packmann.sh</code></p>
+
+<p>Update the program:</p>
+<p><code>$ git pull<code></p>
+
---
+++ packmann.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+echo "<1> Update the Repos"
+echo "<2> Update the System"
+echo "<3> Backup installed programs"
+echo "<4> Restore programs"
+
+read choose
+
+if dnf --version	#For systems with fedora
+then
+	if [ $choose -eq 1 ]
+	then
+		dnf check-update
+
+	elif [ $choose -eq 2 ]
+	then
+		dnf distro-sync
+
+	elif [ $choose -eq 3 ]
+	then
+		rpm -qa > Software_Backup.txt
+		echo "Backup is stored in $(pwd)/Software_Backup.txt"
+   
+ 	elif [ $choose -eq 4 ]
+ 	then
+		if [ -f Software_Backup.txt ]
+		then
+        		for i in $(cat Software_Backup.txt);
+        		do
+                		dnf install -y $i
+                		#echo $i 
+        		done
+		else
+			echo "Keine Backup-Datei gefunden"
+		fi
+
+	fi
+
+elif pacman --version	#For Arch-based systems
+then
+	if [ $choose -eq 1 ]
+	then
+		pacman -Sy
+
+	elif [ $choose -eq 2 ]
+	then
+		pacman -Syu
+
+	elif [ $choose -eq 3 ]
+	then
+		pacman -Qnq > Software_Backup_pacman.txt
+		echo "Backup is stored in $(pwd)/Software_Backup.txt"
+   
+ 	elif [ $choose -eq 4 ]
+ 	then
+		if [ -f Software_Backup_pacman.txt ]
+		then
+        		for i in $(cat Software_Backup.txt);
+        		do
+                		pacman -S --needed --noconfirm $i
+                		#echo $i 
+        		done
+		else
+			echo "Keine Backup-Datei gefunden"
+		fi
+
+	fi
+
+elif yum --version	#For CentOS etc. systems
+then
+	if [ $choose -eq 1 ]
+	then
+		yum check-update
+
+	elif [ $choose -eq 2 ]
+	then
+		yum distro-sync
+
+	elif [ $choose -eq 3 ]
+	then
+		rpm -qa > Software_Backup.txt
+		echo "Backup is stored in $(pwd)/Software_Backup.txt"
+   
+ 	elif [ $choose -eq 4 ]
+ 	then
+		if [ -f Software_Backup.txt ]
+		then
+        		for i in $(cat Software_Backup.txt);
+        		do
+                		yum install -y $i
+                		#echo $i 
+        		done
+		else
+			echo "Keine Backup-Datei gefunden"
+		fi
+
+	fi
+
+elif apt --version	#For Debian based systems
+then
+	if [ $choose -eq 1 ]
+	then
+		apt update
+
+	elif [ $choose -eq 2 ]
+	then
+		apt dist-upgrade
+
+	elif [ $choose -eq 3 ]
+	then
+		apt -list --installed > Software_Backup.txt
+		echo "Backup is stored in $(pwd)/Software_Backup.txt"
+   
+ 	elif [ $choose -eq 4 ]
+ 	then
+		if [ -f Software_Backup.txt ]
+		then
+        		for i in $(cat Software_Backup.txt);
+        		do
+                		apt install -y $i
+                		#echo $i 
+        		done
+		else
+			echo "Keine Backup-Datei gefunden"
+		fi
+
+	fi
+fi
